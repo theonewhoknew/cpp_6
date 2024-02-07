968327d0f56cbfecd94ddc443ecdc9d0c8ac2088
theonewhoknew <theonewhoknew@protonmail.com>
2024-02-07 13:41:18 +0100

    remove extra files

--- ex01/tmp/Serializer.d
+++
@@ -1,5 +0,0 @@
-tmp/Serializer.o: src/Serializer.cpp src/../inc/Serializer.hpp \
- src/../inc/../inc/Data.hpp src/../inc/Data.hpp
-src/../inc/Serializer.hpp:
-src/../inc/../inc/Data.hpp:
-src/../inc/Data.hpp:
--- ex01/tmp/main.d
+++
@@ -1,5 +0,0 @@
-tmp/main.o: src/main.cpp src/../inc/Serializer.hpp \
- src/../inc/../inc/Data.hpp src/../inc/Data.hpp
-src/../inc/Serializer.hpp:
-src/../inc/../inc/Data.hpp:
-src/../inc/Data.hpp:
